863e3f8360b66e264580536b680f63460b3f75c7
Andrew Ryu <andrewryu@outlook.com>
2022-07-18 17:20:22 -0700

    trying to resolve deployment issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import "./App.css";
 import "./index.css";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faCircle,
