b320faed335f340482f62033c75e66ba1923ce5d
Andrew Ryu <andrewryu@outlook.com>
2022-07-19 11:42:38 -0700

    added clear all button

--- src/App.js
+++ src/App.js
@@ -9,19 +9,24 @@ import {
     faChevronRight,
     faPlus,
     faTrashCan,
+    faCircleMinus,
 } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
     const [items, setItems] = useState([
-        // { itemName: "item 1", quantity: 1, isSelected: false },
-        // { itemName: "item 2", quantity: 3, isSelected: true },
-        // { itemName: "item 3", quantity: 2, isSelected: false },
+        // { itemName: "milk", quantity: 1, isSelected: false },
+        // { itemName: "bananas", quantity: 2, isSelected: true },
+        // { itemName: "yogurt", quantity: 1, isSelected: false },
     ]);
+
+    // useEffect(() => {
+    //     localStorage.setItem("items", JSON.stringify(items));
+    // }, [items]);
 
     // create a state object and initialize it with an empty string
     const [inputValue, setInputValue] = useState("");
 
-    const [totalItemCount, setTotalItemCount] = useState(1);
+    const [totalItemCount, setTotalItemCount] = useState(0);
 
     const handleAddButtonClick = () => {
         const newItem = {
@@ -35,6 +40,7 @@ function App() {
 
         // update the state
         setItems(newItems);
+        totalQuantity();
 
         // resets the state
         setInputValue("");
@@ -48,6 +54,7 @@ function App() {
         // returns the object at that index and inc the quantity
         newItems[index].quantity++;
 
+        console.log(newItems);
         setItems(newItems);
         totalQuantity();
     };
@@ -78,6 +85,7 @@ function App() {
             return total + item.quantity;
         }, 0); // starting value should be 0
 
+        console.log(totalItemCount);
         setTotalItemCount(totalItemCount);
     };
 
@@ -87,9 +95,14 @@ function App() {
         setItems((items) => items.filter((_, i) => i !== index));
     };
 
+    const handleClearItem = (index) => {
+        setItems([]);
+        setTotalItemCount(0);
+    };
+
     return (
         <div className="app-background">
-            {/* <h1>Shopping List</h1> */}
+            <h1>Shopping List</h1>
             <div className="main-container">
                 <div className="add-item-box">
                     <input
@@ -152,6 +165,18 @@ function App() {
                     ))}
                 </div>
                 <div className="total">Total: {totalItemCount}</div>
+                <div className="clearAll">
+                    <button
+                        className="clearButton"
+                        onClick={() => handleClearItem()}
+                    >
+                        <FontAwesomeIcon
+                            icon={faCircleMinus}
+                            className="circleMinus"
+                        />
+                        Clear
+                    </button>
+                </div>
             </div>
         </div>
     );
